184daaaf88af97d6709aa7a1d3812f3b9b29236e
Will Toher <will.toher.0@gmail.com>
2021-05-02 13:04:05 -0500

    initial commit

---
+++ tablander-background.js
@@ -0,0 +1,44 @@
+(() => {
+  const getFilteredSites = () =>
+    browser.storage.sync
+      .get("sites")
+      .then(({ sites }) => sites.map((u) => ({ hostContains: u })));
+
+  async function thereCanOnlyBeOne({ tabId, url }) {
+    const { cookieStoreId: containerName } = await browser.tabs.get(tabId);
+
+    const existingTabs = await browser.tabs.query({ url });
+
+    if (!Array.isArray(existingTabs)) return;
+
+    const matchingTabs = existingTabs.filter(
+      (t) => t.id !== tabId && t.cookieStoreId === containerName
+    );
+    console.log({matchingTabs});
+
+    if (matchingTabs[0]) {
+      browser.tabs.remove(tabId);
+      browser.tabs.highlight({
+        populate: false,
+        tabs: [matchingTabs[0].index],
+        windowId: matchingTabs[0].windowId,
+      });
+    }
+  }
+
+  async function registerNavListener() {
+    const filteredSites = await getFilteredSites();
+    if (Array.isArray(filteredSites) && filteredSites.length >= 1) {
+      browser.webNavigation.onBeforeNavigate.addListener(thereCanOnlyBeOne, {
+        url: filteredSites,
+      });
+    }
+  }
+
+  browser.storage.onChanged.addListener(() => {
+    browser.webNavigation.onBeforeNavigate.removeListener(thereCanOnlyBeOne);
+    registerNavListener();
+  });
+
+  registerNavListener();
+})();
---
+++ tablander-options.js
@@ -0,0 +1,19 @@
+function saveOptions(e) {
+  const sites = document
+    .querySelector("#sites")
+    .value.split("\n")
+    .map((s) => s.trim())
+    .filter(Boolean);
+
+  console.log(sites);
+  browser.storage.sync.set({ sites });
+  e.preventDefault();
+}
+
+async function restoreOptions() {
+  const { sites } = await browser.storage.sync.get("sites");
+  document.querySelector("#sites").value = (sites || []).join('\n');
+}
+
+document.addEventListener("DOMContentLoaded", restoreOptions);
+document.querySelector("form").addEventListener("submit", saveOptions);
